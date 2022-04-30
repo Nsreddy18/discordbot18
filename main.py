c353cd0afa12e78d6daa658bb5c098a2908dbe89
N Sujeet Reddy <94231363+Nsreddy18@users.noreply.github.com>
2022-04-30 17:54:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 import praw
 import random
 
-#importing reddit id from env files
 reddit = praw.Reddit(
     client_id=os.environ['client-id'],
     client_secret=os.environ['client-secret'],
@@ -12,14 +11,12 @@ reddit = praw.Reddit(
 )
 
 client=discord.Client();
+
 @client.event
 
 async def on_ready():
-  
-    #shows that the bot is online in the console
-    
     print("we have logged in as {0.user}".format(client))
-    
+ 
 @client.event
 
 async def on_message(message):
@@ -27,16 +24,24 @@ async def on_message(message):
     
     if(message.author == client.user):
         return
-      
+    
     if(message.content.startswith("!r")):
-      subs = reddit.subreddit(str[3:]).top()
-
-      post_to_pick = random.randint(1,100)
-
-      for i in range(0,post_to_pick):
-        submission = next(x for x in subs if not x.stickied)
         
-      await message.channel.send(submission.url)
+      try:
+        subs = reddit.subreddit(str[3:]).hot()
+
+        post_to_pick = random.randint(1,100)
+
+        for i in range(0,post_to_pick):
+          submission = next(x for x in subs if not x.stickied)
+        
+        await message.channel.send(submission.url)
+        
+      except Exception:
+        
+        await message.channel.send('''The subreddit doesn't exist
+ps : check your spelling :)''')
+
 
 my_secret = os.environ['token']
 

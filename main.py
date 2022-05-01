6c20e550af5af5cdb16ac502cdf3c1cc6e2a0f5e
N Sujeet Reddy <94231363+Nsreddy18@users.noreply.github.com>
2022-05-01 16:40:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,30 +11,26 @@ reddit = praw.Reddit(
 )
 
 client=discord.Client();
-
 @client.event
 
 async def on_ready():
     print("we have logged in as {0.user}".format(client))
- 
 @client.event
 
 async def on_message(message):
     str = message.content
-    
     if(message.author == client.user):
         return
-    
-    if(message.content.startswith("!r")):
-        
+    if(message.content.startswith("!r.new")):
       try:
-        subs = reddit.subreddit(str[3:]).hot()
+        print(str[7:])
+        subs = reddit.subreddit(str[7:]).new()
 
         post_to_pick = random.randint(1,100)
 
         for i in range(0,post_to_pick):
           submission = next(x for x in subs if not x.stickied)
-        
+          
         await message.channel.send(submission.url)
         
       except Exception:
@@ -42,8 +38,26 @@ async def on_message(message):
         await message.channel.send('''The subreddit doesn't exist
 ps : check your spelling :)''')
 
+        
+    if(message.content.startswith("!r")):
+      if(message.content.startswith("!r.new")):
+        return
+      try:
+        subs = reddit.subreddit(str[3:]).hot()
+
+        post_to_pick = random.randint(1,100)
+
+        for i in range(0,post_to_pick):
+          submission = next(x for x in subs if not x.stickied)
+          
+        await message.channel.send(submission.url)
+        
+      except Exception:
+        await message.channel.send('''The subreddit doesn't exist
+ps : check your spelling :)''')
 
 my_secret = os.environ['token']
 
 client.run(my_secret)
+
 
